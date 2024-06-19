f1d194f8f174e765eb10ba7c00b60afc5ec50ebe
Sidd3100 <siddraimb@gmail.com>
2024-06-19 09:06:03 +0530

    Landing page completed

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from '../features/product-list/productList'
+import Navbar from '../features/navbar/Navbar'
+
+export function Home() {
+    
+
+    return (
+        <>
+            <Navbar/>
+           <ProductList/> 
+        </>
+    )
+}
